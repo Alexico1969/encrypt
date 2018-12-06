f32c8d2d91a3e3d42f7e0014e322f9539a586d0b
Alexico1969 <34400264+Alexico1969@users.noreply.github.com>
2018-12-06 15:32:10 +0100

    Update function_encrypt.php

--- function_encrypt.php
+++ function_encrypt.php
@@ -9,22 +9,22 @@
 <?php
 
 function encrypt($input , $key){
-	$output = "";
-	$array = str_split($input);
-	foreach ($array as $char) {
-		$iGetAscii = ord($char);
+	$output = "";  // create empty output variable
+	$array = str_split($input); // split the string ($input) into characters --> make an array of it ($array)
+	foreach ($array as $char) {  // loop through $array  (the characters one by one)
+		$iGetAscii = ord($char);  // get the ascii value of the character
 		
-		if ($iGetAscii>64 && $iGetAscii<91){
-			$sEncrypted = chr(((($iGetAscii + $key)-64)%26)+64);
-			$output=$output.$sEncrypted;
-		} elseif ($iGetAscii>96 && $iGetAscii<123){
-			$sEncrypted = chr(((($iGetAscii + $key)-96)%26)+96);
-			$output=$output.$sEncrypted;
+		if ($iGetAscii>64 && $iGetAscii<91){  // if the character is a-z
+			$sEncrypted = chr(((($iGetAscii + $key)-64)%26)+64);  //  ** ENCRYPT ! **
+			$output=$output.$sEncrypted;  // add to $output  (and build the encrypted string this way char by char)
+		} elseif ($iGetAscii>96 && $iGetAscii<123){   // if the character is A-Z
+			$sEncrypted = chr(((($iGetAscii + $key)-96)%26)+96);  //  ** ENCRYPT ! **
+			$output=$output.$sEncrypted;  // add to $output
 		} else {
-			return("* INPUT ERROR *");
+			return("* INPUT ERROR *");  // if there is a non-alphabetic character in the input string
 		}
 	}
-	return $output;	
+	return $output;	 // beëindig de functie door $output terug te geven ($output is nu de complete encrypted string
 }
 
 function decrypt($input , $key){
